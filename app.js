b1f76e70ee790140b15801e2e5932e191b050ac8
AnushanR <anushan.ragunathan@ryerson.ca>
2022-01-10 21:01:53 -0500

    Initialized server and did a Vanilla Request to NASA API

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
 const express = require('express');
+const { get } = require('http');
 const app = express();
+const https = require('https')
+// Using native HTTPS Module to handle GET Request
+app.get("/", function(req,res) {
+
+    const apiKey = "1gY8x93RZ3i7tl4vJFLO0IeQqaEfdOXGXqhAFWKm"
+
+    const url = 'https://api.nasa.gov/planetary/apod?api_key='+apiKey+'&thumbs=True'
+
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+        response.on('data', function(data){
+            //parsing returned data into JSON 
+            const podData = JSON.parse(data)
+            const date = podData.date
+            const title = podData.title
+            const url = podData.thumbnail_url
+            const desc = podData.explanation
+            console.log(podData)
+            console.log(date)
+            console.log(url)
+            res.write("<h1>NASA's Pic of the Day: " + title+"</h2>")
+            res.write("<img src="+url+">")
+            res.write("<p>"+desc+"</p>")
+            res.write("<h2>This is the NASA pic of the day for " + date +"</h2>")
+            res.end();
+        })
+    })
+    
+})
 
 
 
